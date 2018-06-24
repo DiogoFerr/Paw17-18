2997019b7d63a9f3b03ef7d1e427b0cb597fdcf7
number13gmr <number13vsc@gmail.com>
2018-06-24 23:56:32 +0100

    recuperado

--- Models/pacienteModel.js
+++ Models/pacienteModel.js
@@ -105,15 +105,15 @@ module.exports.pacientesAtendidosTriagem = (userid, callback) => {
 
 module.exports.procurarPacientesExames = (callback) => {
     var sql = ("SELECT paciente.*, servico.prioridade, servico.dataEntrada " +
-        "FROM paciente " +
-        "INNER JOIN registo ON paciente.idPaciente = registo.idRegisto " +
-        "INNER JOIN servico ON registo.idRegisto = servico.Registo_idRegisto " +
-        "WHERE (servico.TipoServico_idTipoServico = 2 OR servico.TipoServico_idTipoServico = 4) AND servico.dataSaida IS NULL " +
-        "ORDER BY " +
-        "(CASE " +
-        "WHEN (servico.prioridade = 'Vermelho') THEN 1 " +
-        "WHEN (servico.prioridade = 'Amarelo') THEN 2 " +
-        "WHEN (servico.prioridade = 'Verde') THEN 3 " +
-        "END), servico.dataEntrada");
+            "FROM servico " +
+            "INNER JOIN registo ON servico.Registo_idRegisto = registo.idRegisto " +
+            "INNER JOIN paciente ON registo.paciente_idpaciente = paciente.idPaciente " +
+            "WHERE (servico.TipoServico_idTipoServico = 2 OR servico.TipoServico_idTipoServico = 4) AND servico.dataSaida IS NULL " +
+            "ORDER BY " +
+            "(CASE " +
+            "WHEN (servico.prioridade = 'Vermelho') THEN 1 " +
+            "WHEN (servico.prioridade = 'Amarelo') THEN 2 " +
+            "WHEN (servico.prioridade = 'Verde') THEN 3 " +
+            "END), servico.dataEntrada");
     mysqlModule.query(sql, callback);
 }
